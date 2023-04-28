6a06c697b780b7af8f9e74faff7c175885cc3bdc
Chi-Fu Wang <chifuwang@yahoo.com>
2023-04-28 12:25:22 -0500

    set context to second

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { createBrowserHistory} from 'history';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const history = createBrowserHistory({ basename: '/second'})
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={'/second'}>
     <Routes>
-      <Route path="/*" element={<App />} />
+      <Route history={history} path="/*" element={<App />} />
     </Routes>
     </BrowserRouter>
   </React.StrictMode>
